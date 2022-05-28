9024608eb7c177256bd333cc6d13a719095c3572
Naveen <naveencreater106@gmail.com>
2022-05-28 14:15:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ for drive in D E F G H I J K
 do
 	OUTPUT=$(pwd)
 	echo "${OUTPUT}"
-    cp -r ScriptFiles/ $drive:/
+    	cp -r ScriptFiles/ $drive:/
 	cp start.sh $drive:/
 	cp boom.sh $drive:/
 	cd $drive:/
