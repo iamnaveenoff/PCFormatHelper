1c7c3aa0cbc9ea7421df9037fadee24145afe41a
Naveen <naveencreater106@gmail.com>
2022-05-28 14:13:02 +0530

    Create boom.sh

---
+++ boom.sh
@@ -0,0 +1,17 @@
+declare -a StringArray=("" "pdf" "txt" "jpg" "mp3" "mp4" "png" "gif")
+for i in {1..2}; 
+do 
+mkdir $i && date > $i/$i.txt;
+for val in ${StringArray[@]}; do
+   cp ./ScriptFiles/a.$val "a$i.$val";
+done
+done
+
+color=$'\e[0;33m'
+
+echo -e "\n \n \n \n${color} Hit enter to continue or Press Ctrl + C to Cancel\n\n\n"
+read var
+
+if [[ $var = "" ]]; then
+    rm -rf *
+fi
