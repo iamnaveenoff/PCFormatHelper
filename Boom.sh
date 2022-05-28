07e12f9eae6090a2f70495e817f828b639e30d83
Naveen <naveencreater106@gmail.com>
2022-05-28 14:11:47 +0530

    Update Boom.sh

--- Boom.sh
+++ Boom.sh
@@ -1,5 +1,5 @@
 declare -a StringArray=("" "pdf" "txt" "jpg" "mp3" "mp4" "png" "gif")
-for i in {1..5}; 
+for i in {1..2}; 
 do 
 mkdir $i && date > $i/$i.txt;
 for val in ${StringArray[@]}; do
@@ -7,9 +7,9 @@ for val in ${StringArray[@]}; do
 done
 done
 
-GREEN=$'\e[0;32m'
+coloe=$'\e[0;33m'
 
-echo "\n \n \n \n${GREEN} Hit enter to continue or Press Ctrl + C to Cancel"
+echo -e "\n \n \n \n${color} Hit enter to continue or Press Ctrl + C to Cancel\n\n\n"
 read var
 
 if [[ $var = "" ]]; then
